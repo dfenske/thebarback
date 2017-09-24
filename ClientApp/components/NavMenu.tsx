7c831ed98c6ccc8e55f027579e3142fa8713f4a5
Dani Fenske <daniellef@payscale.com>
2017-09-24 16:18:59 -0700

    Adding some preliminary styling!

--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -3,38 +3,29 @@ import { Link, NavLink } from 'react-router-dom';
 
 export class NavMenu extends React.Component<{}, {}> {
   public render() {
-    return <div className='main-nav'>
-      <div className='navbar navbar-inverse'>
-        <div className='navbar-header'>
-          <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
-            <span className='sr-only'>Toggle navigation</span>
-            <span className='icon-bar'></span>
-            <span className='icon-bar'></span>
-            <span className='icon-bar'></span>
-          </button>
-          <Link className='navbar-brand' to={'/'}>The Barback</Link>
-        </div>
-        <div className='clearfix'></div>
-        <div className='navbar-collapse collapse'>
-          <ul className='nav navbar-nav'>
-            <li>
-              <NavLink to={'/'} exact activeClassName='active'>
-                <span className='glyphicon glyphicon-glass'></span> Search
-                            </NavLink>
-            </li>
-            <li>
-              <NavLink to={'/counter'} activeClassName='active'>
-                <span className='glyphicon glyphicon-tree-conifer'></span> About the Creators
-                            </NavLink>
-            </li>
-            <li>
-              <NavLink to={'/fetchdata'} activeClassName='active'>
-                <span className='glyphicon glyphicon-pencil'></span> Contribute
-                            </NavLink>
-            </li>
+    return (
+      <nav>
+        <div className="nav-wrapper blue-grey darken-4">
+          <a href="#" className="brand-logo"><span className="glyphicon glyphicon-glass"></span>The Barback</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+              <NavLink to={'/'} exact className="nav-link">
+                  <span className='glyphicon glyphicon-glass'></span> Search
+                </NavLink>
+              </li>
+              <li>
+              <NavLink to={'/counter'} className='nav-link'>
+                  <span className='glyphicon glyphicon-tree-conifer'></span> About the Creators
+                </NavLink>
+              </li>
+              <li>
+              <NavLink to={'/fetchdata'} className='nav-link'>
+                  <span className='glyphicon glyphicon-pencil'></span> Contribute
+                </NavLink>
+              </li>
           </ul>
         </div>
-      </div>
-    </div>;
+      </nav>
+    );
   }
 }
