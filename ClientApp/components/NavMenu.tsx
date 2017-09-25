ed6747578074b971defbc45eeb932a73d1e58bf1
Dani Fenske <daniellef@payscale.com>
2017-09-24 21:59:27 -0700

    More pretty homepage stuff

--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -6,23 +6,24 @@ export class NavMenu extends React.Component<{}, {}> {
     return (
       <nav>
         <div className="nav-wrapper blue-grey darken-4">
-          <a href="#" className="brand-logo"><span className="glyphicon glyphicon-glass"></span>The Barback</a>
+          <a href="#" className="brand-logo"><i className="large material-icons">local_bar</i>
+            The Barback</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li>
-              <NavLink to={'/'} exact className="nav-link">
-                  <span className='glyphicon glyphicon-glass'></span> Search
-                </NavLink>
-              </li>
-              <li>
-              <NavLink to={'/counter'} className='nav-link'>
-                  <span className='glyphicon glyphicon-tree-conifer'></span> About the Creators
-                </NavLink>
-              </li>
-              <li>
-              <NavLink to={'/fetchdata'} className='nav-link'>
-                  <span className='glyphicon glyphicon-pencil'></span> Contribute
-                </NavLink>
-              </li>
+            <li title="Search">
+              <NavLink to={'/'} exact className="nav-link" >
+                <i className="large material-icons">search</i>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={'/counter'} className='nav-link' title="About">
+                <i className="large material-icons">accessibility</i>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={'/fetchdata'} className='nav-link' title="Contribute">
+                <i className="large material-icons">create</i>
+              </NavLink>
+            </li>
           </ul>
         </div>
       </nav>
