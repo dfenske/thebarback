cc7e78ecb5cfd2e4dfed22949764ad709c3007a7
PSSEA\daniellef <daniellef@payscale.com>
2017-10-14 12:48:24 -0700

    Switch between recipes with button

--- ClientApp/components/Search.tsx
+++ ClientApp/components/Search.tsx
@@ -86,10 +86,18 @@ class Recipe extends React.Component<IRecipeProps, {}> {
 }
 
 interface ISearchState {
-  recipe?: IRecipe
+  recipe?: IRecipe,
+  recipeId?: number,
 }
 
 export class Search extends React.Component<RouteComponentProps<{}>, ISearchState> {
+  changeRecipe(): any {
+    if (this.state.recipeId === 0) {
+      this.getRecipe(1);
+    } else {
+      this.getRecipe(0);
+    }
+  }
 
   constructor() {
     super();
@@ -97,19 +105,22 @@ export class Search extends React.Component<RouteComponentProps<{}>, ISearchStat
   }
 
   componentWillMount() {
-    this.getRecipe(1).then((result) => {
-      this.setState({ recipe: result });
-    });
+    this.getRecipe(1);
   }
 
-  private getRecipe(id: number): Promise<any> {
-    return fetch(`/recipe/get/${id}`).then((result) => result.json());
+  private getRecipe(id: number): void {
+    fetch(`/recipe/get/${id}`).then((result) => result.json()).then((result) => {
+      this.setState({ recipe: result, recipeId: id });
+    });
   }
 
   public render() {
     if (this.state.recipe) {
       return (
-        <Recipe recipe={this.state.recipe} />
+        <div>
+          <a className="waves-effect waves-light btn" onClick={this.changeRecipe.bind(this)}>Switch recipe</a>
+          <Recipe recipe={this.state.recipe} />
+        </div>
       );
     }
     return null;
